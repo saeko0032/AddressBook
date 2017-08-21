c7087ed6038a5c7df101500f9b6e1c7afc6111d2
Saeko0032 <saeko0032@gmail.com>
2017-08-21 14:04:12 -0700

    homework

--- app/src/main/java/com/example/fukuisaeko/addressbook/ContactsFragment.java
+++ app/src/main/java/com/example/fukuisaeko/addressbook/ContactsFragment.java
@@ -1,15 +1,21 @@
 package com.example.fukuisaeko.addressbook;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.fukuisaeko.addressbook.addressbook.ContactsAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ContactsFragment extends Fragment {
+
+    private ContactsAdapter adapter;
 
     public ContactsFragment() {
     }
@@ -17,6 +23,14 @@ public class ContactsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        setHasOptionsMenu(true);
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        FloatingActionButton add = (FloatingActionButton)view.findViewById(R.id.addButton);
+        adapter = new ContactsAdapter();
+      //  return inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
     }
 }
--- app/src/main/java/com/example/fukuisaeko/addressbook/addressbook/ContactsAdapter.java
+++ app/src/main/java/com/example/fukuisaeko/addressbook/addressbook/ContactsAdapter.java
@@ -1,25 +1,64 @@
 package com.example.fukuisaeko.addressbook.addressbook;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.fukuisaeko.addressbook.R;
+import com.example.fukuisaeko.addressbook.addressbook.data.DatabaseDescription;
 
 /**
  * Created by fukuisaeko on 2017-08-16.
  */
 
-public class ContactsAdapter extends RecyclerView.Adapter {
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder> {
+
+    // inner class
+    class ContactsViewHolder extends RecyclerView.ViewHolder {
+        TextView contactTextView;
+        long id;
+        Cursor cursor;
+
+        ContactsViewHolder(View itemViews) {
+            super(itemViews);
+            //set textid
+        }
+
+        public void setRowID(long rowID) {
+        // set db id
+        }
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public ContactsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // create  a simple list item
+        // inflate the android.R.layout.simple_list_item_1 layout
+        View view = inflater.inflate(R.layout.simple_list_item_1, parent, false);
+        ContactsViewHolder viewHolder = new ContactsViewHolder(view);
+        return viewHolder;  // return current item's ViewHolder
 
     }
+
+    public void onBindViewHolder(ContactsViewHolder holder, int position) {
+        cursor.moveToPosition(position);
+        holder.tvContact.setText(cursor.getString(cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_NAME)));
+        holder.setRowID(cursor.getLong(cursor.getColumnIndex(DatabaseDescription.Contact._ID)));
+    }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        Cursor cursor;
+        int rows = cursor.getCount():
+
+        return rows;
     }
+
 }
