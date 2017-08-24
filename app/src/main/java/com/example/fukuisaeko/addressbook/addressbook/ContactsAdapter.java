96f599f183097b951b3a176642fcd6c784594a28
Saeko0032 <saeko0032@gmail.com>
2017-08-23 22:27:53 -0700

    change layout file

--- app/src/main/java/com/example/fukuisaeko/addressbook/addressbook/ContactsAdapter.java
+++ app/src/main/java/com/example/fukuisaeko/addressbook/addressbook/ContactsAdapter.java
@@ -2,6 +2,7 @@ package com.example.fukuisaeko.addressbook.addressbook;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,48 +18,77 @@ import com.example.fukuisaeko.addressbook.addressbook.data.DatabaseDescription;
 
 public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder> {
 
-    // inner class
-    class ContactsViewHolder extends RecyclerView.ViewHolder {
-        TextView contactTextView;
-        long id;
-        Cursor cursor;
+    // interface implemented by ContactsFragment to respond
+    // when the user touches an item in the RecyclerView
+    public interface ContactClickListener {
+        void onClick(Uri contactUri);
+    }
 
-        ContactsViewHolder(View itemViews) {
-            super(itemViews);
-            //set textid
+    // nested subclass of RecyclerView.ViewHolder used to implement
+    // the view-holder pattern in the context of a RecyclerView
+    public class ContactsViewHolder extends RecyclerView.ViewHolder {
+        public final TextView textView;
+        private long rowID;
+
+        // configures a RecyclerView item's ViewHolder
+        public ContactsViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(android.R.id.text1);
+
+            // attach listener to itemView
+            itemView.setOnClickListener(
+                    new View.OnClickListener() {
+                        // executes when the contact in this ViewHolder is clicked
+                        @Override
+                        public void onClick(View view) {
+                            clickListener.onClick(DatabaseDescription.Contact.buildContactUri(rowID));
+                        }
+                    }
+            );
         }
 
+        // set the database row ID for the contact in this ViewHolder
         public void setRowID(long rowID) {
-        // set db id
+            this.rowID = rowID;
         }
     }
 
+    // ContactsAdapter instance variables
+    private Cursor cursor = null;
+    private final ContactClickListener clickListener;
+
+    // constructor
+    public ContactsAdapter(ContactClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+
+    // sets up new list item and its ViewHolder
     @Override
     public ContactsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
-
-        // create  a simple list item
         // inflate the android.R.layout.simple_list_item_1 layout
-        View view = inflater.inflate(R.layout.simple_list_item_1, parent, false);
-        ContactsViewHolder viewHolder = new ContactsViewHolder(view);
-        return viewHolder;  // return current item's ViewHolder
-
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                android.R.layout.simple_list_item_1, parent, false);
+        return new ContactsViewHolder(view); // return current item's ViewHolder
     }
 
+    // sets the text of the list item to display the search tag
+    @Override
     public void onBindViewHolder(ContactsViewHolder holder, int position) {
         cursor.moveToPosition(position);
-        holder.tvContact.setText(cursor.getString(cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_NAME)));
         holder.setRowID(cursor.getLong(cursor.getColumnIndex(DatabaseDescription.Contact._ID)));
+        holder.textView.setText(cursor.getString(cursor.getColumnIndex(
+                DatabaseDescription.Contact.COLUMN_NAME)));
     }
 
-
+    // returns the number of items that adapter binds
     @Override
     public int getItemCount() {
-        Cursor cursor;
-        int rows = cursor.getCount():
-
-        return rows;
+        return (cursor != null) ? cursor.getCount() : 0;
     }
 
+    // swap this adapter's current Cursor for a new one
+    public void swapCursor(Cursor cursor) {
+        this.cursor = cursor;
+        notifyDataSetChanged();
+    }
 }
